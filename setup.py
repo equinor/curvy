c99d939298756d20ebd72f0dac951dcb537c4f7a
Joachim Blaafjell Holwech <joachim.holwech@gmail.com>
2019-02-13 10:52:09 +0000

    Fixed an error in install script

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='curvy',
     version='0.1dev',
     packages=['curvy',],
     license='MIT',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     keyswords='smooth curve curves interpolation swap forward prices price python forecast spline electricity gas market maximum smoothness',
     author='Joachim Holwech',
-    author='jholw@equinor.com',
-    packages=find_packages(),
+    author_email='jholw@equinor.com',
     install_requires=[
         'numpy',
         'scipy',
